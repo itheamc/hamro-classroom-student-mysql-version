2d30ef4681b2c71eff0d992bf27721ef06003e94
Amit Chaudhary <itheamc@gmail.com>
2021-08-22 09:07:39 +0545

    Version 1.0.0 - Assignment View Bugs Fixed

--- app/src/main/java/com/itheamc/hamroclassroom_student/handlers/RequestHandler.java
+++ app/src/main/java/com/itheamc/hamroclassroom_student/handlers/RequestHandler.java
@@ -221,7 +221,7 @@ public class RequestHandler {
 
     // GET REQUEST
     public static Request assignmentGetRequestBySchoolIdAndClass(@NonNull String _userId, @NonNull String _schoolId, @NonNull String _class) {
-        return new Request.Builder().url(PathHandler.ASSIGNMENTS_PATH + _schoolId + "___" + _class).addHeader("user", _userId).get().build();
+        return new Request.Builder().url(PathHandler.ASSIGNMENTS_PATH + _schoolId + "___" + _class).addHeader("by", "school").addHeader("user", _userId).get().build();
     }
 
     // POST REQUEST
